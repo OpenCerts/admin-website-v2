697d02986c81989b42750a624bb4cc75ddf2ecd3
Chen Ren <siachenren@gmail.com>
2021-06-29 16:06:54 +0800

    fix: package script and test script

--- integration/metamask-init.mjs
+++ integration/metamask-init.mjs
@@ -8,7 +8,7 @@ export const metamaskInit = async () => {
       executablePath: process.env.PUPPETEER_EXEC_PATH,
       headless: false, // must be false, so can test with metamask extension
       defaultViewport: null,
-      slowMo: process.argv[2] || 10,
+      slowMo: process.argv[2] || 0,
     });
     const metamask = await dappeteer.getMetamask(browser, {
       seed: "indicate swing place chair flight used hammer soon photo region volume shuffle",
