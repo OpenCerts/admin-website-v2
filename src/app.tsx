7cd2ae964beace31e191f37f581e39eefd0562a2
ChenRen <siachenren@gmail.com>
2021-07-08 17:44:03 +0800

    feat: add metamask pending page

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,26 @@
 declare let window: any;
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import "./app.css";
 import { MainPage } from "./page/main-page";
 import { MetaMaskError } from "./page/metamask-error";
+import { MetaMaskPending } from "./page/metamask-pending";
 
 const App: FunctionComponent = () => {
+  const [metamaskConnected, setMetamaskConnected] = useState(false);
+  useEffect(() => {
+    checkMetaMaskAccount();
+  });
+
+  const checkMetaMaskAccount = async () => {
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+    accounts.length > 0 ? setMetamaskConnected(true) : setMetamaskConnected(false);
+  };
+
   if (!window.ethereum) {
     return <MetaMaskError />;
   }
 
-  return <MainPage />;
+  return metamaskConnected ? <MainPage /> : <MetaMaskPending />;
 };
 
 export default App;
