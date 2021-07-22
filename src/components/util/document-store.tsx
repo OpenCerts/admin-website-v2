5290b8c546f12d4ac64ff8b66162085e1ab01cba
ChenRen <siachenren@gmail.com>
2021-07-22 18:50:39 +0800

    fix: update retrieval document store

--- src/components/util/document-store.tsx
+++ src/components/util/document-store.tsx
@@ -1,21 +1,21 @@
-import { connectWallet, getWalletNetwork } from "./wallet";
+import { connectWallet, getSigner, getWalletNetwork } from "./wallet";
 
 export const getDocumentStores = async (): Promise<Array<Record<string, unknown>>> => {
   try {
     const provider = await connectWallet();
-
+    const signer = await getSigner();
     // Topics = deploy document store method,
     // The topics can be found in Etherscan transaction logs
+    // topic[0] = OwnershipTransferred function, topic[1] = _from address, topic[2] = _to address
     const logInfo = {
       fromBlock: "earliest",
       toBlock: "latest",
       topics: [
         "0x8be0079c531659141344cd1fd0a4f28419497f9722a3daafe3b4186f6b6457e0",
         "0x0000000000000000000000000000000000000000000000000000000000000000",
-        "0x000000000000000000000000c84b0719a82626417c40f3168513dfabdb6a9079",
+        `0x000000000000000000000000${(await signer.getAddress()).replace("0x", "").toLowerCase()}`,
       ],
     };
-
     const getLogs = await provider.getLogs(logInfo);
     if (getLogs.length > 0) {
       return getLogs.map((logInformation) => {
