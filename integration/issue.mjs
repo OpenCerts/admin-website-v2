f625c4e18c01fb5d669b43a03990a9df6f993111
Tng Zi Xiang <tngzixiang@gmail.com>
2023-10-02 22:55:57 +0800

    chore: fix tests

--- integration/issue.mjs
+++ integration/issue.mjs
@@ -1,46 +1,43 @@
-import expect from "expect-puppeteer";
+import { expect } from "chai";
+import { sleep, waitForAndClickElement, waitForElement } from "./utils.mjs";
 
 export const issue = async (metamask, browser) => {
   try {
     const page = await browser.newPage();
-    await page.goto("http://localhost:5000/");
+    await page.goto("http://localhost:5173/", { waitUntil: "networkidle" });
 
     // START - approve application once, subsequent tests no longer need
-    await metamask.approve({ allAccounts: true });
+    await metamask.approve();
     await page.bringToFront();
     // END - approve application once, subsequent tests no longer need
-    await page.waitForSelector("[data-testid='connectToWallet']", {
-      visible: true,
-    });
-    await page.click("[data-testid='connectToWallet']");
+    await waitForAndClickElement(page, "[data-testid='connectToWallet']");
 
-    await page.waitForSelector("[aria-controls='react-autowhatever-document-store']", {
-      visible: true,
-    });
-    await page.focus("[aria-controls='react-autowhatever-document-store']");
-    await page.keyboard.type("0xC84b0719A82626417c40f3168513dFABDB6A9079");
+    await waitForAndClickElement(page, ".react-autosuggest__input");
+    await page.type(".react-autosuggest__input", "0xC84b0719A82626417c40f3168513dFABDB6A9079");
 
     // Test user successfully issue the transaction
-    await page.waitForSelector("[data-testid='issue-certificate']", {
-      visible: true,
-    });
-    await page.focus("[data-testid='issue-certificate']");
-    await page.keyboard.type("0xbabf7285760d279e6dfa9c37d97bd3f77025a0be0e74119ff24ac9ee0739c60b");
+    await waitForElement(page, "[data-testid='issue-certificate']");
+    await page.type(
+      "[data-testid='issue-certificate']",
+      "0xbabf7285760d279e6dfa9c37d97bd3f77025a0be0e74119ff24ac9ee0739c60b"
+    );
 
-    await page.waitForSelector("[data-testid='issue-certificate-btn']", {
-      visible: true,
-    });
-    await page.click("[data-testid='issue-certificate-btn']");
+    await waitForAndClickElement(page, "[data-testid='issue-certificate-btn']");
 
-    await page.waitFor(1500);
+    await sleep(1500);
     await metamask.confirmTransaction();
     await page.bringToFront();
-    await page.waitFor(3000);
+    await sleep(3000);
 
-    await expect(page).toMatchElement("[data-testid='issue-log']", {
-      text: `Document/Document Batch with hash 0xbabf7285760d279e6dfa9c37d97bd3f77025a0be0e74119ff24ac9ee0739c60b has been issued to 0xC84b0719A82626417c40f3168513dFABDB6A9079.`,
-      visible: true,
+    await waitForElement(page, "[data-testid='issue-log']");
+    const issueLog = await page.evaluate(() => {
+      const el = document.querySelector("[data-testid='issue-log']");
+      return el.textContent;
     });
+
+    await expect(issueLog).to.have.string(
+      "Document/Document Batch with hash 0xbabf7285760d279e6dfa9c37d97bd3f77025a0be0e74119ff24ac9ee0739c60b has been issued to 0xC84b0719A82626417c40f3168513dFABDB6A9079."
+    );
 
     await page.close();
     console.info("✅  Issue success");
--- integration/metamask-init.mjs
+++ integration/metamask-init.mjs
@@ -1,19 +1,54 @@
-import puppeteer from "puppeteer";
 import dappeteer from "@chainsafe/dappeteer";
+import { addNetwork } from "./addNetwork.mjs";
+import { sleep } from "./utils.mjs";
 
 export const metamaskInit = async () => {
   try {
-    const browser = await dappeteer.launch(puppeteer, {
-      args: ["--no-sandbox"],
-      executablePath: process.env.PUPPETEER_EXEC_PATH,
-      headless: false, // must be false, so can test with metamask extension
-      defaultViewport: null,
-      slowMo: process.argv[2] || 0,
+    const browser = await dappeteer.launch({
+      metaMaskVersion: "v10.31.0",
+      automation: "puppeteer",
+      headless: false,
+      puppeteerOptions: {
+        args: ["--no-sandbox"],
+        executablePath: process.env.PUPPETEER_EXEC_PATH,
+        defaultViewport: null,
+        slowMo: process.argv[2] || 0,
+      },
     });
-    const metamask = await dappeteer.getMetamask(browser, {
+
+    await sleep(2000);
+    const pages = await browser.pages();
+    for (const page of pages) {
+      if (page.url().startsWith("chrome-extension")) {
+        await page.reload();
+      }
+    }
+
+    // Get metamask
+    const metamask = await dappeteer.setupMetaMask(browser, {
       seed: "indicate swing place chair flight used hammer soon photo region volume shuffle",
+      showTestNets: true,
     });
-    await metamask.switchNetwork("localhost");
+
+    // Add network
+    // https://github.com/ChainSafe/dappeteer/blob/b79ab4c74fab87747933d8f428624dcbffc3dd19/test/basic.spec.ts#L117-L119
+    const dappPage = await browser.newPage();
+    await dappPage.goto("https://github.com", { waitUntil: "networkidle" });
+
+    const addNetworkPromise = dappPage.evaluate(addNetwork);
+    try {
+      setTimeout(async () => {
+        await metamask.acceptAddNetwork(true);
+      }, 2000);
+    } catch (e) {
+      // ignore error
+      for (const page of pages) {
+        if (page.url().startsWith("chrome-extension")) {
+          await page.reload();
+        }
+      }
+    }
+    await addNetworkPromise;
 
     console.info("✅ Metamask account init success");
     return { metamask, browser };
